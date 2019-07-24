832780826024c14400f3240106f224a4effd4972
aman4411 <amanmehta4411@gmail.com>
2019-07-24 15:11:30 +0530

    login acttivity added

---
+++ app/src/main/java/com/example/casehearing/Login.java
@@ -0,0 +1,25 @@
+package com.example.casehearing;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
