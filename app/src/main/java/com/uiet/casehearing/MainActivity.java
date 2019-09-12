7829be6ba94b3dba8bb28a31bb080677efb6369b
aman4411 <amanmehta4411@gmail.com>
2019-09-12 21:03:39 +0530

    crash fixed

--- app/src/main/java/com/uiet/casehearing/MainActivity.java
+++ app/src/main/java/com/uiet/casehearing/MainActivity.java
@@ -336,8 +336,6 @@ public class MainActivity extends AppCompatActivity
             startActivity(i);
         }
 
-
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
